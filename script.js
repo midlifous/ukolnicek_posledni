4257e2d0650de1fb6c59aa74d4bb0d2b1c627018
Michal Vyleta <=>
2024-12-16 20:24:59 +0100

    Znemožňování tlačítek

--- script.js
+++ script.js
@@ -28,9 +28,17 @@ function pridat()
 function smazat(cislo){
   number = cislo;
   document.getElementById("in" + cislo).className = "animace";
+  let tlacitka = document.getElementsByTagName("button");
+  for (let i = 0; i < tlacitka.length; i++) {
+    tlacitka[i].disabled = true;
+  }
   setTimeout(mazani, 5000);
 }
 function mazani(){
   let mazanaVec = document.getElementById("in" + number);
   mazanaVec.remove();
+  let tlacitka = document.getElementsByTagName("button");
+  for (let i = 0; i < tlacitka.length; i++) {
+    tlacitka[i].disabled = false;
+  }
 }
